36f470fabc7cf6582c03f3808dd3979ce143f21a
Dan Johansson <danjohansson98@gmail.com>
2022-11-21 17:23:41 +0100

    Wrote unit test for find_neighbour function using boids

--- test_boid.py
+++ test_boid.py
@@ -1,4 +1,5 @@
-from boid import boid
+from boid import Boid
+from find_neighbours import findNeighbours
 
 # Unit test for boid.py
 
@@ -7,10 +8,38 @@ from boid import boid
 
 
 def test_updatePosition():
-    testBoid = boid(0,0,0,1,-1,0,[],[],0)
+    testBoid = Boid(0,0,0,1,-1,0)
     testBoid.updatePosition()
 
     assert testBoid.x == 1, "x position not updated correctly"
     assert testBoid.y == -1, "y position not updated correctly"
     assert testBoid.z == -0, "z position not updated correctly"
+    
+def test_findNeighbours():
+    r = 2
+    L = 10
+    testBoid1 = Boid(0,0,0,1,-1,0)
+    testBoid2 = Boid(1,0,0,1,-1,0)
+    testBoid3 = Boid(2,2,2,1,-1,0)
+    
+    boids = [testBoid1, testBoid2, testBoid3]
+    
+    neighbours = findNeighbours(boids, r, L)
+    
+    for boid in boids:
+        boid.smallflock = [boids[neighbour] for neighbour in neighbours[boids.index(boid)]]
+        
+    print(testBoid1)
+    print(testBoid2)
+    print(testBoid3)
+    print(testBoid1.smallflock)
+    print(testBoid2.smallflock)
+    
+    assert testBoid1 in testBoid1.smallflock, "smallflock not updated correctly"
+    assert testBoid2 in testBoid1.smallflock, "smallflock not updated correctly"
+    assert testBoid3 not in testBoid1.smallflock, "smallflock not updated correctly"
+    
+    
+    
+    
     
