8a2b52b40ae2051a5d874d830c7fee55591b995b
Dan Johansson <danjohansson98@gmail.com>
2022-11-24 09:48:33 +0100

    Made food method more OPP oriented

--- food.py
+++ food.py
@@ -3,16 +3,17 @@ import numpy as np
 
 # Class food
 class Food():
-    def __init__(self, x, y, z):
+    def __init__(self, x, y, z, L):
         self.x = x
         self.y = y
         self.z = z
-        
+        self.L = L
+   
 # Function for food spawning
-def food_spawn(number_of_food, food_list,L):
+def food_spawn(self, number_of_food, food_list):
     for i in range(number_of_food):
-        x = np.random.random() * L
-        y = np.random.random() * L
-        z = np.random.random() * L
+        x = np.random.random() * self.L
+        y = np.random.random() * self.L
+        z = np.random.random() * self.L
         food_list.append(Food(x,y,z))
     return food_list
