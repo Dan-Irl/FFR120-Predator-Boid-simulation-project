18bac6c378a7977c3ff598cd92d69b1d40c15599
Dan Johansson <danjohansson98@gmail.com>
2022-11-26 18:24:34 +0100

    Wrote function for finding the closest target

--- find_neighbours.py
+++ find_neighbours.py
@@ -4,10 +4,21 @@
 # The boundingBox is the size of the area that the individuals are in. This is used to wrap the individuals around the edges of the area. 
 # The function returns a list of lists. Each list contains the indices of the individuals that are neighbours of the individual with the same index as the list.
 # List if neighbours includes the individual itself.
-from scipy.spatial import cKDTree
+from scipy.spatial import KDTree
 import numpy as np
 
 def findNeighbours(individuals:list,range:int,boundingBox:int) -> list:
+    """Finds all neighbours for each individual given a range."""
     positions = np.array([individual.getPosition() for individual in individuals])
-    tree = cKDTree(positions,boxsize=boundingBox)
+    tree = KDTree(positions,boxsize=boundingBox)
     return tree.query_ball_point(positions, range)
+
+# Function that finds bad neighbours
+
+def findTarget(Individuals:list,Targets:list,range:int,boundingBox:int) -> list:
+    """Finds the closest target for each individual given a list of targets and a range."""
+    positions = np.array([individual.getPosition() for individual in Individuals])
+    targets = np.array([target.getPosition() for target in Targets])
+    tree = KDTree(targets,boxsize=boundingBox)
+    
+    return [Targets[index[0]] for index in tree.query_ball_point(positions, range)]
