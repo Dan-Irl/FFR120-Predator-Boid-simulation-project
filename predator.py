02d4f4f73cda94b1351ac223306c8c11ead5998c
jespeols <99250520+jespeols@users.noreply.github.com>
2022-12-01 12:13:44 +0100

    Expanded predator class
    Mainly pushing to be able to work on my new computer

--- predator.py
+++ predator.py
@@ -1,58 +1,95 @@
 # class that defines the predators and their behaviour
+import numpy as np
 
 class Predator:
-    def __init__(self,x,y,z):
-       self.x = x
-       self.y = y
-       self.z = z
-       self.vx = 0
-       self.vy = 0
-       self.vz = 0
-       self.chasing = False     #indicates whether the predator is chasing a prey
+    def __init__(self,x:float,y:float,z:float,v0:float,dt:float):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.v0 = v0
+        self.dt = dt
+        self.vx = v0
+        self.vy = v0
+        self.vz = v0
+        self.health = 100
+        self.chasing = False     
+        self.chased_boid = None
+        self.resting = False
        
+    def get_position(self):
+        return (self.x,self.y,self.z)
+
+    def get_health(self):
+        return self.health
+
+    def get_chasing(self):
+        return self.chasing
+
+    def get_chased_boid(self):
+        return self.chased_boid
+
     # function that updates the position of the predator
-    def updatePosition(self):
-        self.x += self.x + self.vx
-        self.y += self.y + self.vy
-        self.z += self.z + self.vz
+    def update_position(self):
+        self.x += self.vx*self.dt
+        self.y += self.vy*self.dt
+        self.z += self.vz*self.dt
 
     # function that updates the velocity of the predator
-    def updateVelocity(self, boids, predators):
-        # if the predator is not chasing a prey, it will move randomly
-        if self.chasing == False:
-            self.vx = random.uniform(-1,1)
-            self.vy = random.uniform(-1,1)
-            self.vz = random.uniform(-1,1)
-        # if the predator is chasing a prey, it will move towards the prey
+    def update_velocity(self):
+        if self.resting == True:
+            self.vx = 0
+            self.vy = 0
+            self.vz = 0
         else:
-            # find the closest prey
-            closest = boids[0]
-            for boid in boids:
-                if distance(self, boid) < distance(self, closest):
-                    closest = boid
-            # move towards the closest prey
-            self.vx = closest.x - self.x
-            self.vy = closest.y - self.y
-            self.vz = closest.z - self.z
+            # if the predator is not chasing a prey, it will move randomly
+            if self.chasing == False:
+                self.vx = np.random.uniform(-1,1)
+                self.vy = np.random.uniform(-1,1)
+                self.vz = np.random.uniform(-1,1)
+
+            else:  # if the predator is chasing a prey, it will move towards the prey
+                # align velocity vector with prey position -> move towards prey
+                self.vx = chased_boid.x - self.x
+                self.vy = chased_boid.y - self.y
+                self.vz = chased_boid.z - self.z
+
+            # normalize the velocity
+            v_norm = np.linalg.norm([self.vx, self.vy, self.vz])
+            self.vx = self.vx*self.v0/v_norm
+            self.vy = self.vy*self.v0/v_norm
+            self.vz = self.vz*self.v0/v_norm
+
+    # immobilize predator for a certain amount of time
+    def rest():
+        self.resting = True
+
+    # function that checks whether the predator is resting
+    def check_resting(self):
+        return self.resting
     
+    def awaken(self):
+        self.resting = False
+
     # function that checks whether the predator is chasing a prey
-    def checkChasing(self, boids):
-        # if the predator is not chasing a prey, it will check if there is a prey in its sensing radius
-        if self.chasing == False:
-            for boid in boids:
-                if distance(self, boid) < r_B:
-                    self.chasing = True
-                    break
-        # if the predator is chasing a prey, it will check if the prey is still in its sensing radius
+    def check_chasing(self, boid_neighbours):
+        if len(neighbours) > 0:     # check if there are any boids in range
+            self.chasing = True
+            self.chased_boid = neighbours[0]     # assumes sorted by distance
+        else:                           # if the predator is chasing a prey
+            self.chasing = False
+    
+    # function that feeds the predator upon health every time step
+    def feed(self):
+        self.health += 10
+
+    # function that decays the predator's health every time step
+    def health_decay(self):
+        self.health -= 1*self.dt
+
+    # function that checks whether the predator is dead
+    def check_dead(self):
+        if self.health <= 0:
+            return True
         else:
-            for boid in boids:
-                if distance(self, boid) > r_B:
-                    self.chasing = False
-                    break
-    
-    # function that checks whether the predator has caught a prey
-    def checkCaught(self, boids):
-        for boid in boids:
-            if distance(self, boid) < r_S:
-                boids.remove(boid)
-    
+            return False
+
--- test_predator.py
+++ test_predator.py
@@ -1,6 +1,78 @@
 from predator import Predator
+from boid import Boid
+import numpy as np
 
 # Unit test for predator.py
 
 # Check here how to write tests (install pytest first):
 # https://docs.pytest.org/en/latest/
+
+def test_get_health():
+    test_predator = Predator(0,0,0,1,1)
+    assert test_predator.get_health() == 100, "Health not initialized correctly"
+
+
+def test_get_position():
+    test_predator = Predator(-10,10,100,1,1)
+    assert test_predator.get_position() == (-10,10,100), "Position not initialized correctly"
+
+
+def test_feed():
+    test_predator = Predator(0,0,0,1,1)
+    test_predator.feed()
+
+    assert test_predator.health == 110, "Health not updated correctly"
+
+def test_get_chasing():
+    test_predator = Predator(0,0,0,1,1)
+    assert test_predator.get_chasing() == False, "Chasing not initialized correctly"
+
+def test_check_chasing():
+    ## test boid out of range ##
+    test_boids = [Boid(100,100,100,1,1,1,1,1,1,1)] 
+    # check for range, should be False
+
+
+    test_predator = Predator(0,0,0,1,1)
+    test_predator.check_chasing(test_boids)
+
+    assert test_predator.chasing == False, "Predator should not be chasing"
+    assert test_predator.chased_boid == None, "Predator should not have a chased boid"
+
+    ## test boid in range ##
+    test_boids = [Boid(0,0,0,1,1,1,1,1,1,1)] 
+    # check for range, should be True
+
+
+    test_predator = Predator(0,0,0,1,1)
+    test_predator.check_chasing(test_boids)
+
+    assert test_predator.chasing == True, "Predator should be chasing"
+    assert test_predator.chased_boid == test_boids[0], "Predator should have a chased boid"
+
+def test_get_chased_boid():
+    test_predator = Predator(0,0,0,1,1)
+    assert test_predator.get_chased_boid() == None, "Chased boid not initialized correctly"
+
+def test_update_position():
+    test_predator = Predator(0,0,0,1,1)
+    test_predator.update_position()
+
+    assert test_predator.x == 1, "x position not updated correctly"
+    assert test_predator.y == 1, "y position not updated correctly"
+    assert test_predator.z == 1, "z position not updated correctly"
+
+def test_update_velocity():
+    # test boid out of range
+    test_boids = [Boid(100,100,100,1,1,1,1,1,1,1)] 
+    # check for range, should be False
+
+
+    test_predator = Predator(0,0,0,1,1)
+    test_predator.update_velocity()
+
+    assert np.abs(test_predator.vx) <= 1, "x velocity not updated correctly"
+    assert np.abs(test_predator.vy) <= 1, "y velocity not updated correctly"
+    assert np.abs(test_predator.vz) <= 1, "z velocity not updated correctly"
+
+    # test boid in range, see that velocity is aligned with boid
