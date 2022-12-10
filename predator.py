278531f3080ae725624e83fbe714bfdb24803a37
Jespeols <jesperolsson9@hotmail.com>
2022-12-10 13:25:01 +0100

    I am pushing commits without double-checking

--- predator.py
+++ predator.py
@@ -54,8 +54,8 @@ class Predator:
     def updateVelocity(self):
         """function that updates the velocity of the predator"""
         if self.resting == True:
-            self.vx = 0.25*np.random.normal(self.vy,abs(self.vz*0.5))
-            self.vy = 0.25*np.random.normal(self.vy,abs(self.vz*0.5))
+            self.vx = 0.25*np.random.normal(self.vx,abs(self.vx*0.5))
+            self.vy = 0.25*np.random.normal(self.vy,abs(self.vy*0.5))
             self.vz = 0.25*np.random.normal(self.vz,abs(self.vz*0.5))
         else:
             # if the predator is not chasing a prey, it will move randomly
